1edd30fbbea6d2d3b3161f2554348fe00bec3426
Arash Kayedpour <arash.kayedpour@gmail.com>
2021-12-29 11:42:42 -0500

    fibonacci tailrec vs regular recursion

---
+++ src/main/scala/recursion/Fibonacci.scala
@@ -0,0 +1,29 @@
+package recursion
+
+object Fibonacci extends App{
+
+  import scala.annotation.tailrec
+
+  def fibonacci(n: Int): Long = {
+    @tailrec
+    def fibo(counter: Int = 0, prev1: Long = 0, prev2: Long = 1): Long = {
+      if (n <= 1) n
+      else if (counter + 2 <= n) fibo(counter + 1, prev2, prev2 + prev1)
+      else prev2
+    }
+
+    fibo()
+  }
+
+  def fibonacciRec(n: Int): Long = {
+    if (n <= 1) n
+    else fibonacciRec(n - 1) + fibonacciRec(n - 2)
+  }
+
+  println(fibonacci(15))
+  println(fibonacciRec(15))
+
+  // with the second version you will get a stackoverflow error if you try bigger numbers
+
+
+}

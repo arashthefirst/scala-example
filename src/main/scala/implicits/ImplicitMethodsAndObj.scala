ec9503d9f0cd5f376647ceb1269eede7ea4270f9
Arash Kayedpour <arash.kayedpour@gmail.com>
2021-12-29 13:18:36 -0500

    implicit conversion

---
+++ src/main/scala/implicits/ImplicitMethodsAndObj.scala
@@ -0,0 +1,22 @@
+package implicits
+
+object ImplicitMethodsAndObj extends App {
+
+  type Predicate = Person => Boolean
+  case class Person(name: String, id: Long)
+  implicit val predicate: Predicate = p => p.name == "John" && p.id == 1
+  implicit val p: Person = Person("John", 1)
+
+
+  def printPerson(implicit p: Person): Unit = {
+    println(p)
+  }
+
+  def testPerson(implicit person: Person, predicate: Predicate): Boolean = {
+    predicate(person)
+  }
+
+  printPerson
+  println(testPerson)
+
+}

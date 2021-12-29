a5a7cd166c3be01e5e5956a78fee6a68eaf503a0
Arash Kayedpour <arash.kayedpour@gmail.com>
2021-12-29 12:28:23 -0500

    function definitions

---
+++ src/main/scala/function/Functions.scala
@@ -0,0 +1,42 @@
+package function
+
+object Functions extends App {
+
+  /*
+   the code below will be compiled into regular java methods
+   example add1:
+
+     int add1(int a , int b ) {
+         return a + b;
+     }
+
+      and
+
+      add3:
+
+      Function2<Integer,Integer,Integer> add3() {
+        return (a,b) -> a + b ;
+      }
+   */
+  def add1(a: Int, b: Int): Int = a + b
+
+  def add2: (Int, Int) => Int = (a, b) => a + b
+
+  def add3 = (a: Int, b: Int) => a + b
+
+  def add4: (Int, Int) => Int = (a: Int, b: Int) => a + b
+
+  /*
+  the following code will be compiled to
+  private final Function2<Integer,Integer,Integer> add4 = (a,b) -> a + b
+
+   */
+  val add5: (Int, Int) => Int = (a, b) => a + b
+  val add6 = (a: Int, b: Int) => a + b
+
+
+
+  val adds = List[(Int, Int) => Int](add1, add2, add3, add4, add5, add6)
+  adds.map(_ (5, 5)).foreach(println)
+
+}

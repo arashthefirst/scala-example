b79fac75e913f18713e18def92ce3c9a1182698b
Arash Kayedpour <arash.kayedpour@gmail.com>
2021-12-29 20:33:29 -0500

    implicit

--- src/main/scala/typesys/ViewBound.scala
+++ src/main/scala/typesys/ViewBound.scala
@@ -11,15 +11,28 @@ object ViewBound extends App {
     Person(fn, id.toLong)
   }
 
-  def register[T <% Person](t: T): Person = {
+  def register[T <% Person](t: T): Person = { //view bounds ( <% ) is deprecated use the other register method
     println(t)
     t // <<== scala uses implicit conversion
+  }
+
+  def register2[T](t: T)(implicit ev: T => Person): Person = {
+    println(t)
+    t // implicit conversion
   }
 
   val p1 = register("Arash,1")
   val p2 = register((1, "Arash"))
 
+
   println(p1)
   println(p2)
+
+  val p3 = register2("Arash,1")
+  val p4 = register2((1, "Arash"))
+
+
+  println(p3)
+  println(p4)
 
 }

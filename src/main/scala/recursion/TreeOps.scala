0ca45376e9fa9dc6e346cbd958f9387b5e4a759d
Arash Kayedpour <arash.kayedpour@gmail.com>
2021-12-31 16:09:43 -0500

    tree

---
+++ src/main/scala/recursion/TreeOps.scala
@@ -0,0 +1,37 @@
+package recursion
+
+object TreeOps extends App {
+
+  trait Tree[+T]
+
+  case class Node[T](value: T, left: Tree[T] = Empty, right: Tree[T] = Empty) extends Tree[T]
+
+  object Empty extends Tree[Nothing]
+
+  val tree = Node(1, Node(2, Node(4)), Node(3, Node(5)))
+
+  //conventional recursive function
+  def sum(tree: Tree[Int]): Int = tree match {
+    case Empty => 0
+    case Node(v, l, r) => v + sum(l) + sum(r)
+  }
+
+  def max(tree: Tree[Int]): Int = {
+
+    def loop(nodes: Tree[Int] = tree, max: Int = Int.MinValue): Int = nodes match {
+      case Empty => max
+      case Node(v, l, r) =>
+        val leftMax = loop(l, if (v > max) v else max)
+        val rightMax = loop(r, if (v > max) v else max)
+        if (leftMax > rightMax) leftMax else rightMax
+    }
+
+    if (tree == Empty) throw new IllegalArgumentException("empty list")
+    loop()
+
+  }
+
+  println(sum(tree))
+  println(max(tree))
+
+}

92199cee271bb086ebca7c765d3f053627df5ef9
Arash Kayedpour <arash.kayedpour@gmail.com>
2021-12-31 17:20:25 -0500

    map function for tree

--- src/main/scala/recursion/TreeOps.scala
+++ src/main/scala/recursion/TreeOps.scala
@@ -6,11 +6,13 @@ object TreeOps extends App {
 
   case class Node[T](value: T, left: Tree[T] = Empty, right: Tree[T] = Empty) extends Tree[T]
 
-  object Empty extends Tree[Nothing]
+  object Empty extends Tree[Nothing] {
+    override def toString: String = s" ()"
+  }
 
   val tree = Node(1, Node(2, Node(4)), Node(3, Node(5)))
 
-  //conventional recursive function
+  //conventional recursive function, non of the functions below are tail recursive, will work on it
   def sum(tree: Tree[Int]): Int = tree match {
     case Empty => 0
     case Node(v, l, r) => v + sum(l) + sum(r)
@@ -31,7 +33,13 @@ object TreeOps extends App {
 
   }
 
+  def map[A, B](tree: Tree[A])(f: A => B): Tree[B] = tree match {
+    case Empty => Empty
+    case Node(v, l, r) => Node(f(v), map(l)(f), map(r)(f))
+  }
+
   println(sum(tree))
   println(max(tree))
+  println(tree)
 
 }
